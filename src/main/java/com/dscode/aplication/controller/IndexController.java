a0d5f15252a8db0edb1f5d7492330f57e7ede8f0
David <david.cabello.info@gmail.com>
2021-11-30 12:24:23 +0100

    error index solucionado

--- src/main/java/com/dscode/aplication/controller/IndexController.java
+++ src/main/java/com/dscode/aplication/controller/IndexController.java
@@ -2,18 +2,16 @@ package com.dscode.aplication.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class IndexController {
 	
 	private final static Logger logger= LoggerFactory.getLogger(IndexController.class);
 	
-	@Autowired
-	// private Saludo cb;
 	
 	private Long contadorIntentos = 0L;
 	
